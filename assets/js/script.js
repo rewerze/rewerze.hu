3dc9543f6b10d686a3430676ad2233026bd8b016
rewerze <gamermod801@gmail.com>
2024-03-12 17:40:37 +0100

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,13 @@ function copyToClipboard(text) {
   document.body.removeChild(name);
 
   const button = document.querySelector("button");
-  button.classList.remove("dc-idle");
+  button.classList.remove("dc");
   button.classList.add("btn-outline-success");
   button.innerHTML = "Név kimásolva";
 
   setTimeout(() => {
     button.classList.remove("btn-outline-success");
-    button.classList.add("dc-idle");
+    button.classList.add("dc");
     button.innerHTML = "Discord";
   }, 2000);
 }
