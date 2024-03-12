cf82c84db5f072e59a3bbaa8031e339ed56359f0
rewerze <gamermod801@gmail.com>
2024-03-12 12:09:51 +0100

    bug fix & more content

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function copyToClipboard(text) {
-    const name = document.createElement('textarea');
-    name.value = text;
-    document.body.appendChild(name);
-    name.select();
-    document.execCommand('copy');
-    document.body.removeChild(name);
-  
-    const button = document.querySelector('button');
-    button.classList.remove('btn-outline-danger');
-    button.classList.add('btn-outline-success');
-    button.innerHTML = 'Név kimásolva';
-  }
+  const name = document.createElement("textarea");
+  name.value = text;
+  document.body.appendChild(name);
+  name.select();
+  document.execCommand("copy");
+  document.body.removeChild(name);
+
+  const button = document.querySelector("button");
+  button.classList.remove("btn-outline-danger");
+  button.classList.add("btn-outline-success");
+  button.innerHTML = "Név kimásolva";
+}
