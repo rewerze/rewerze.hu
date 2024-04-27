464367f87a39c7e4490f1120a10e7b3658eb90f4
rewerze <gamermod801@gmail.com>
2024-04-27 07:48:53 +0200

    reworked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Home:search
+document
+  .getElementById("search-button")
+  .addEventListener("click", function (event) {
+    var searchTerm = document.getElementById("search-input").value.trim(); // Keresési kifejezés lekérése és whitespace eltávolítása
+
+    if (searchTerm !== "") {
+      // Ha van beírt kifejezés
+      var searchUrl = "/kereses/" + encodeURIComponent(searchTerm); // Összeállítjuk a keresési URL-t
+
+      // Átirányítás a keresési URL-re
+      window.location.href = searchUrl;
+    } else {
+      alert("Kérlek írj be valamit a kereséshez!"); // Ha nincs beírt kifejezés, jelenítse meg az üzenetet
+    }
+  });
+
+//Social:Discord
 function copyToClipboard(text) {
   const name = document.createElement("textarea");
   name.value = text;
@@ -9,11 +27,15 @@ function copyToClipboard(text) {
   const button = document.querySelector("button");
   button.classList.remove("discord");
   button.classList.add("btn-outline-success");
-  button.innerHTML = "Név kimásolva";
+  document.getElementById("discord-img").src =
+    "../assets/images/discord_green.svg";
+  document.getElementById("discord").innerHTML = "Név kimásolva!";
 
   setTimeout(() => {
     button.classList.remove("btn-outline-success");
     button.classList.add("discord");
-    button.innerHTML = "Discord";
+    document.getElementById("discord-img").src =
+      "../assets/images/discord_purple.svg";
+    document.getElementById("discord").innerHTML = "Discord";
   }, 1500);
 }
