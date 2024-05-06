4d705d43a41e92e415978d72dda640fbd2e95698
rewerze <gamermod801@gmail.com>
2024-05-06 17:06:18 +0200

    search szar

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,36 +22,26 @@ function copyToClipboard(text) {
     document.getElementById("discord").innerHTML = "Discord";
   }, 1500);
 }
+//Nav: Search
+function Search() {
+  document
+    .getElementById("endpointForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      var endpoint = document.getElementById("endpointInput").value.trim();
+      if (endpoint !== "") {
+        window.location.href = endpoint;
+      } else {
+        alert("Kérjük, írja be az oldal végpontját!");
+      }
+    });
+}
 
-//Nav
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      };
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
+//Nav:Import
+function importNav(path) {
+  fetch(path)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("target").innerHTML = html;
+    });
 }
