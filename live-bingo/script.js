4eb582073709f021ba8ca7c9d0dae8bbe62e5a53
rewerze <gamermod801@gmail.com>
2024-09-26 20:32:38 +0200

    fix

--- live-bingo/script.js
+++ live-bingo/script.js
@@ -1,10 +1,10 @@
         // Bingó cellák generálása
         const phrases = [
-            "TÉNYLEG LIVE LEGYEN", "Minecraft Dungeons", "Új fa típus", "Egyedi zene", "Új spin-off",
-            "Youtuber promo", "1+ óra", "Marketplace dolgok", "Mob vote vége", "Fejlesztést segítő újítások",
-            "Minecraft movie hírek", "Szülinap", "1.22 update", "Absolute cinema 👏🎬", "Kollab",
-            "Minecraft Legends", "Új boss", "Bedrock DLC", "MEME potenciál", "Community show pre-show",
-            "Minecraft Netflix hírek", "Vote vesztes", "Education Edition dolgok", "Combat update", "Váratlan bejelentés"
+            "Youtuber promo", "Minecraft movie hírek", "Cringe", "Hardcore újitások", "Új spin-off",
+            "TÉNYLEG LIVE LEGYEN", "1+ óra", "Marketplace dolgok", "Mob vote vége", "Fejlesztést segítő újítások",
+            "Minecraft Dungeons", "Szülinap 🎂", "1.22-es update bejelentése", "Absolute cinema 👏🎬", "Sneak peak a kisebb droppokról 👀",
+            "Fletching table", "1.21.2 release date", "Bedrock DLC", "MEME potenciál", "Community show pre-show",
+            "Minecraft Netflix hírek", "Vote vesztes", "Education Edition", "Váratlan bejelentés", "End update"
         ];
 
         const board = document.getElementById('bingo-board');
@@ -16,7 +16,7 @@
 
             for (let i = 0; i < 25; i++) {
                 const cell = document.createElement('div');
-                cell.classList.add('bingo-cell');
+                cell.classList.add('bingo-cell', 'w-100');
                 cell.innerText = phrases[i];
                 cell.addEventListener('click', () => cell.classList.toggle('marked')); // Kattintásra jelölés
                 board.appendChild(cell);
